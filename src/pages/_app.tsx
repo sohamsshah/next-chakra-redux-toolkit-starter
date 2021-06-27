8f5753ea70984f9e98903f32f2885c987d4301c8
sohamsshah <sohamshah456@gmail.com>
2021-06-27 16:57:49 +0530

    update file structure

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,7 +3,7 @@ import { ChakraProvider, theme } from '@chakra-ui/react'
 import { Provider } from 'react-redux'
 import type { AppProps } from 'next/app'
 
-import store from '../store/store'
+import store from '../redux/store'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
