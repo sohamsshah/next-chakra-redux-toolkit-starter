a8b42a036cf3c432dd554509d59227782db3f51c
sohamsshah <sohamshah456@gmail.com>
2021-06-27 16:58:11 +0530

    change FS

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -11,11 +11,4 @@ export type AppState = ReturnType<typeof store.getState>
 
 export type AppDispatch = typeof store.dispatch
 
-export type AppThunk<ReturnType = void> = ThunkAction<
-  ReturnType,
-  AppState,
-  unknown,
-  Action<string>
->
-
 export default store
